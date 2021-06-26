7d0b5766dace81feb6a1480ec485f5ca3786f18d
mohammedbellogithub <52636929+mohammedbellogithub@users.noreply.github.com>
2021-06-26 14:57:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OverloadAss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var u1 = new User() { FullName = "Segun Ojo", Age = 20 , Level = 3};
+            var u2 = new User() { FullName = "Segun Ojo", Age = 89 , Level = 5};
+            var result = --u2;
+
+            if (u1.Age == u1.Level)
+            {
+                Console.WriteLine("Hello from true");
+            }
+            else
+            {
+                Console.WriteLine("Hello from false");
+            }
+
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("End of Main");
+        }
+    }
+}
+ 
---
+++ User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadAss
+{
+    class User
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+        public int Level { get; set; }
+
+        public static User operator +(User first, User second)
+        {
+            return new User() { Age = first.Age + second.Age };
+        }
+        public static bool operator >(User first, User second)
+        {
+            return first.Age > second.Age;
+        }
+        public static bool operator <(User first, User second)
+        {
+            return first.Age < second.Age;
+        }
+        public static User operator --(User age)
+        {
+            return new User() { Age = --age.Age };
+        }
+        public static User operator ++(User age)
+        {
+            return new User() { Age = ++age.Age };
+        }
+        public static bool operator true(User user) => user.Age == user.Level;
+        public static bool operator false(User user) => user.Age != user.Level;
+          
+        public override string ToString()
+        {
+            return $"FullName: {FullName} Age: {this.Age} Level:{this.Level}";
+        }
+    }
+}
